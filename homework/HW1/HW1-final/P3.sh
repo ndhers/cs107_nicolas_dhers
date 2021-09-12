609470d2315054c11e766ac1230ec85468624321
Nicolas Dhers <nicolasdhers@dhcp-140-247-103-82.fas.harvard.edu>
2021-09-11 22:11:36 -0400

    Adding Part 3

---
+++ homework/HW1/HW1-final/P3.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+grep -c '[0-9]' apollo13.txt > appolo_out.txt
+grep --help 2>&1 | grep "\--count"
+ls -f | grep -c .py
+ls -lR | grep -c "\-......\-\-."
+ls -l | grep -c "[-d]......\-\-."
