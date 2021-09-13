586420b680c2562d80bc5bb512e6d4b99fa79c99
Nicolas Dhers <nicolasdhers@dhcp-140-247-103-82.fas.harvard.edu>
2021-09-13 17:45:32 -0400

    pushing exercise_2.sh

--- pair_programming/PP2/exercise_2.sh
+++ pair_programming/PP2/exercise_2.sh
@@ -1,5 +1,6 @@
 #!/bin/bash                                                                     
-#coder:Nicolas Dhers, Xuliang Guo, Haochen Yang, Xiang Bai                      #sharer:Xiang Bai 
+#coder:Nicolas Dhers, Xuliang Guo, Haochen Yang, Xiang Bai
+#sharer:Xiang Bai 
 #listener:Nicolas Dhers, Xuliang Guo, Haochen Yang, Xiang Bai                   
 
 find . -type f -perm -u=x -maxdepth 1 
